8d61c3489408a2531c17cce2cfcebe02201d024e
Sam <freivaldsam@gmail.com>
2022-01-24 19:56:11 +0200

    objects.js

--- src/objects.js
+++ src/objects.js
@@ -3,4 +3,48 @@ var cat = {
     "name": "Гармония",
     "color": "Черепаховый"
 } ;
-cat["name"];
+cat["name"];var cat = {
+    "legs": 3,
+    "name": "Гармония",
+    "color": "Черепаховый" 
+    };
+    
+    cat["name"];
+    
+    var dog = {
+        "name": "Оладушек",
+        "age": 6,
+        "color": "Черепаховый",
+        "bark": "Гав, тяф, тяф!" 
+        };
+        var cat = {
+        "name": "Гармония",
+        "age": 8,
+        "color": "белый"
+        };
+        cat.name
+        'Гармония'
+        Object.keys(dog);
+        
+        ['name', 'age', 'color', 'bark']
+        Object.keys(cat);
+        ['name', 'age', 'color']
+        var dinosaurs = [
+            {name: "Тираннозавр рекс", period: "Верхнемеловый" },
+             {name: "Стегозавр" , period: "верхнеюрский" },
+            {name: "Платеозавр" , period: "Триасовый"}
+        ];
+        dinosaurs[0];
+        dinosaurs[0] ["name"];
+        'Тираннозавр рекс'
+        dinosaurs[1].period;
+        'верхнеюрский'
+        var owedMoney = {};
+owedMoney["Джимми"] = 5;
+owedMoney["Анна"] = 7;
+owedMoney["Джимми"];
+5
+owedMoney["Джимми"] +=3;
+owedMoney["Джимми"];
+8
+owedMoney;
